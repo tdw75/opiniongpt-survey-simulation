e389da7d47e1d94edad76a021098b90c73e62c8f
twilliams <tristan.williams@zalando.de>
2025-01-25 13:04:47 +0100

    add functions to read variables from pdf and convert to individual strings

---
+++ src/data/read.py
@@ -0,0 +1,31 @@
+import os
+import pickle
+
+import PyPDF2
+
+
+def pdf_to_strings(directory: str, page_nums: list[int] = None) -> dict[int, str]:
+    file_name = "WVS7_Codebook_Variables_report_V6.0.pdf"
+
+    with open(os.path.join(directory, file_name), 'rb') as f:
+        reader = PyPDF2.PdfReader(f)
+        page_nums = page_nums or range(1, len(reader.pages))
+        pages = {i: reader.pages[i-1].extract_text() for i in page_nums}
+
+    return pages
+
+
+def pickle_pages(directory: str, pages: dict[int, str]) -> None:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    for n, text in pages.items():
+        with open(os.path.join(directory, f"page{n}.pkl"), "wb") as f:
+            pickle.dump(text, f)
+
+
+def unpickle_pages(directory: str, page_nums: list[int]) -> dict[int, str]:
+    pages = {}
+    for n in page_nums:
+        with open(os.path.join(directory, f"page{n}.pkl"), "rb") as f:
+            pages[n] = pickle.load(f)
+    return pages
