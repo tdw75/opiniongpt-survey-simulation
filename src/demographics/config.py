c0a67babb412a54fb0b0454398c7e0ee4e7c3c57
twilliams <tristan.williams@zalando.de>
2025-01-29 12:25:10 +0100

    add more demographic subgroups

---
+++ src/demographics/config.py
@@ -0,0 +1,9 @@
+from src.demographics.age import OldPeople, Over30
+from src.demographics.country import Germany, UnitedStates, MiddleEast, LatinAmerica
+from src.demographics.sex import Female, Male
+from src.demographics.political_leaning import Progressive, Conservative
+
+ALL_COUNTRIES = [Germany, UnitedStates, MiddleEast, LatinAmerica]
+ALL_SEX = [Male, Female]
+ALL_AGE = [Over30, OldPeople]
+ALL_POLITICAL = [Progressive, Conservative]
--- src/demographics/country.py
+++ src/demographics/country.py
@@ -4,6 +4,10 @@ from dataclasses import dataclass
 from src.demographics.base import BaseSubGroup
 
 
+# todo: split on country of birth or country of survey?
+# Q266, Q267, Q268 own country of birth, mother's, father's
+
+
 @dataclass
 class Country(BaseSubGroup, ABC):
     COLUMN = "B_COUNTRY_ALPHA"
@@ -21,12 +25,18 @@ class UnitedStates(Country):
 
 @dataclass
 class MiddleEast(Country):
-    VALUES = {"TUR"}
+    # todo: add all countries
+    VALUES = {"CYP", "EGY", "IRN", "IRQ", "JOR", "LBN", "TUR"}
+    # definition from https://en.wikipedia.org/wiki/Middle_East
+    # not found: Bahrain, Israel, Kuwait, Oman, Palestine, Qatar, Saudi Arabia, Syria, UAE, Yemen
 
 
 @dataclass
 class LatinAmerica(Country):
-    VALUES = {"MEX", "BRA",}
+    # todo: add all countries
+    VALUES = {"ARG", "BOL", "BRA", "CHL", "COL", "ECU", "GTM", "MEX", "NIC", "PER", "PRI", "URY", "VEN"}
+    # definition from https://en.wikipedia.org/wiki/Latin_America
+    # not found: Costa Rica, Cuba, Dominican Republic, El Salvador, Honduras, Panama, Paraguay
 
 
 if __name__ == "__main__":
