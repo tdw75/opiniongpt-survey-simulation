447f75f79bf2fc12a25c6aef7a1b8a00fc828339
Tristan Williams <tristan.williams@zalando.de>
2025-03-26 17:56:33 +0100

    rename Progressive subgroup to Liberal to align with OpinionGPT adapter name

--- src/demographics/config.py
+++ src/demographics/config.py
@@ -1,9 +1,9 @@
 from src.demographics.age import OldPeople, Over30
 from src.demographics.country import Germany, UnitedStates, MiddleEast, LatinAmerica
 from src.demographics.sex import Female, Male
-from src.demographics.political_leaning import Progressive, Conservative
+from src.demographics.political_leaning import Liberal, Conservative
 
 ALL_COUNTRIES = [Germany, UnitedStates, MiddleEast, LatinAmerica]
 ALL_SEX = [Male, Female]
 ALL_AGE = [Over30, OldPeople]
-ALL_POLITICAL = [Progressive, Conservative]
+ALL_POLITICAL = [Liberal, Conservative]
