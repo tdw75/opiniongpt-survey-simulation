5e2888538b416daf67d6680f25a70a35171542d9
Tristan Williams <tristan.williams@zalando.de>
2025-03-28 09:53:33 +0100

    fix unit test

--- tests/unit/prompting/test_system.py
+++ tests/unit/prompting/test_system.py
@@ -5,7 +5,7 @@ from src.prompting.system import build_persona_message
 def test_build_persona_message():
     message = build_persona_message(Female)
     expected = f"""
-    Please answer all following questions as if you are a person that identifies as female.
+    Please answer all questions as if you are a person that identifies as female.
     Make sure your responses align as closely as possible to the responses that someone from this 
     demographic is likely to give.
     """
