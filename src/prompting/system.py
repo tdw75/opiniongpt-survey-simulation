0984e4851e6d55bf8e7d542d045f169481e651f5
Tristan Williams <tristan.williams@zalando.de>
2025-04-25 15:50:21 +0200

    adjust format

--- src/prompting/system.py
+++ src/prompting/system.py
@@ -19,7 +19,7 @@ def build_survey_context_message() -> str:
     # Instructions
     * For each question, select a single answer from the set of available responses 
     * Your response should be in the format of f'response: {number} - {response}, explanation: {explanation}' and contain nothing else 
-        - e.g. 'response: 2 - Agree, explanation: because I ... '
+        - e.g. 'response: 2 - Agree \n explanation: because I ... '
     
     # Examples
     
