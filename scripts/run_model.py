57c3040bd5913560186c064cdaa07d75ff1356e9
Tristan Williams <tristan.williams@zalando.de>
2025-04-30 16:12:56 +0200

    fix question_num argument

--- scripts/run_model.py
+++ scripts/run_model.py
@@ -38,10 +38,10 @@ def main(
     print(model)
 
     survey_questions = load_survey(directory, filename, question_format)
-    survey_questions = get_single_question(survey_questions)  # todo: delete after debugging
+    survey_questions = get_single_question(survey_questions, question_num)  # todo: delete after debugging
     system_prompt = build_survey_context_message()
     respondents = simulate_whole_survey(
-        model, tokenizer, survey_questions, by, system_prompt, hyperparams=kwargs, num=question_num
+        model, tokenizer, survey_questions, by, system_prompt, hyperparams=kwargs
     )
     survey_run = {  # todo: add rest of metadata
         "metadata": {
