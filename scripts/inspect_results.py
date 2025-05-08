d6408954c400cd1dc73d54032fbe9fb0a0e31516
Tristan Williams <tristan.williams@zalando.de>
2025-05-08 20:25:10 +0200

    fix file name

--- scripts/inspect_results.py
+++ scripts/inspect_results.py
@@ -13,12 +13,12 @@ from src.simulation.utils import get_nth_newest_file, print_results
 def main(run_id: str | int = 0, directory: str = "data_files"):
 
     if isinstance(run_id, str):
-        file_name = run_id
+        file_name = os.path.join(directory, run_id)
     elif isinstance(run_id, int):
         file_name = get_nth_newest_file(run_id, directory)
     else:
         raise TypeError(f"run_id is type {type(run_id)} but must be either int or str")
-    with open(os.path.join(directory, file_name)) as f:
+    with open(file_name) as f:
         results = json.load(f)
         print_results(results)
 
