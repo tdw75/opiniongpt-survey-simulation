c4a8adeb46d14c785a7e59f3f16911f60b097d8b
Tristan Williams <tristan.williams@zalando.de>
2025-05-21 14:49:30 +0200

    fix typo

--- src/prompting/system.py
+++ src/prompting/system.py
@@ -30,7 +30,7 @@ def build_survey_context_message() -> str:
     "
     
     For each question, just select a single answer from the set of available responses.
-    Please copy the answer exactly as written in the 'Available responses' list abover. 
+    Please copy the answer exactly as written in the 'Available responses' list above. 
     Do not change spelling or capitalisation. 
     For example:
     Agree strongly"
