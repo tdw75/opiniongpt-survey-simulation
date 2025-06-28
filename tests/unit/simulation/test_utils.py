d2db9e1d5407b03fa9199484404c870459c11295
Tristan Williams <tristan.williams@zalando.de>
2025-06-28 10:59:40 +0200

    update tests

--- tests/unit/simulation/test_utils.py
+++ tests/unit/simulation/test_utils.py
@@ -1,11 +1,6 @@
 import pandas as pd
-import pytest
 
-from src.simulation.utils import (
-    filter_survey_subset,
-    mark_is_scale_flipped,
-    get_batches
-)
+from src.simulation.utils import filter_survey_subset, mark_is_scale_flipped
 
 
 def test_filter_survey_subset():
@@ -16,20 +11,6 @@ def test_filter_survey_subset():
     pd.testing.assert_series_equal(
         survey_subset["number"], pd.Series(expected_qnums, name="number")
     )
-
-
-@pytest.mark.parametrize("sample_size", (4, 5))
-def test_get_batches(sample_size):
-    expected_batch1 = [[{"message1": "content"}], [{"message2": "content"}]]
-    expected_batch2 = [[{"message3": "content"}], [{"message4": "content"}]]
-    expected = [expected_batch1, expected_batch2]
-
-    if sample_size == 5:
-        expected.append([[{"message5": "content"}]])
-
-    messages_batched = [[{f"message{i}": "content"}] for i in range(1, sample_size + 1)]
-    for i, batch in enumerate(get_batches(messages_batched, batch_size=2)):
-        assert batch == expected[i]
 
 
 def test_mark_is_scale_flipped():
