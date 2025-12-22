cffe8b70243746a51a3a693a65d8f35a82a2ae03
Tristan Williams <tristan.williams@zalando.de>
2025-12-22 16:12:15 +0100

    use 7B llama 2 as base model

--- src/simulation/models.py
+++ src/simulation/models.py
@@ -39,7 +39,7 @@ adapters: list[AdapterName] = list(bias_to_subreddit.keys())
 
 MODEL_DIRECTORY = {
     "phi": "unsloth/Phi-3-mini-4k-instruct",
-    "llama": "meta-llama/Llama-2-13b-chat-hf",
+    "llama": "meta-llama/Llama-2-7b-chat-hf",
     # "llama3": "meta-llama/Meta-Llama-3-8B-Instruct",
 }
 
