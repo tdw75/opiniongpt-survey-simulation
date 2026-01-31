2f5ab454bdd0da7beeb9ebeba1e27e090d99fe93
tdw75 <tdw75@outlook.com>
2026-01-31 23:26:15 +0100

    rename parameter for consistency across scripts

--- scripts/results_to_csv.py
+++ scripts/results_to_csv.py
@@ -16,19 +16,19 @@ from src.analysis.results import (
 )
 
 
-def main(directory: str, file_root: str):
+def main(directory: str, simulation_name: str):
     """Convert survey results from JSON to CSV format for cleaning."""
-    simulation_directory = os.path.join(directory, "results", file_root)
+    simulation_directory = os.path.join(directory, "results", simulation_name)
     variables = pd.read_csv(os.path.join(directory, "variables", "variables.csv"))
-    is_folder = os.path.isdir(os.path.join(simulation_directory, file_root))
+    is_folder = os.path.isdir(os.path.join(simulation_directory, simulation_name))
     if is_folder:
-        results = load_survey_results_batch(file_root, simulation_directory)
+        results = load_survey_results_batch(simulation_name, simulation_directory)
         df = survey_results_to_df_batch(results, variables)
     else:
-        results = load_survey_results(f"{file_root}.json", simulation_directory)
+        results = load_survey_results(f"{simulation_name}.json", simulation_directory)
         df = survey_results_to_df(results, variables)
 
-    df.to_csv(os.path.join(simulation_directory, f"{file_root}.csv"))
+    df.to_csv(os.path.join(simulation_directory, f"{simulation_name}.csv"))
 
 
 if __name__ == "__main__":
